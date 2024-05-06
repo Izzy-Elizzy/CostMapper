a74454fb472d001eb52795dd713173b5fc86b09d
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-05-06 19:30:30 -0400

    Initial commit

---
+++ deploy_map.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template_string
+
+import folium
+
+import folium
+import geopandas as gpd
+
+m = folium.Map(location=[38.8, -96.8], zoom_start=6)
+
+counties = gpd.read_file("c:\\Users\\izzye\\Downloads\\Countys\\gz_2010_us_050_00_5m\\gz_2010_us_050_00_5m.shp")
+
+folium.GeoJson(
+    counties,
+    name="counties",
+    fill_opacity=0.2,
+    line_weight=1,
+    zoom_on_click=True
+).add_to(m)
+
+app = Flask(__name__)
+
+@app.route("/iframe")
+def iframe():
+    """Embed a map as an iframe on a page."""
+    # m = folium.Map()
+
+    # set the iframe width and height
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
+
+    return render_template_string(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head></head>
+                <body>
+                    <h1>Using an iframe</h1>
+                    {{ iframe|safe }}
+                </body>
+            </html>
+        """,
+        iframe=iframe,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
